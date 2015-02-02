3ebfa15084b7b61526d0912feac1ae64e6426972
Jordan Gumm <gumm1jn@cmich.edu>
2015-02-02 12:07:10 -0500

    added initial base terminal files

---
+++ js/terminal.js
@@ -0,0 +1,22 @@
+$('#screen input').focus();
+$('#screen input').keypress(function (e) {
+    var key = e.which;
+    if(key == 13) {
+        
+        var $this = $(this), val = $this.val()
+        $this.focus().val('');
+        if (val === "python hello_world.py") {
+            $('#screen p').text('hello to you!');
+        } else if (val === "ls") {
+            $('#screen p').text("'hello_world.py'");
+        } else if (val == "help") {
+            $('#screen p').text('ls - enter to display directory file commands');
+        } else {
+            if (val.indexOf(".py") != -1) {
+                $('#screen p').text("python (.py) scripts must have prefix command 'python' ---  try 'python <command>'");
+            } else {
+                $('#screen p').text('unrecognized command: use "help" command for, well, help!');
+            }
+        }
+    }
+});
