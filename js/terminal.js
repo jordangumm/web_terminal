525ae1e6016e0a16838f7f00b0b5f4745473a0eb
Jordan Gumm <gumm1jn@cmich.edu>
2015-02-02 12:18:40 -0500

    changed some prompt syntax

--- js/terminal.js
+++ js/terminal.js
@@ -8,14 +8,14 @@ $('#screen input').keypress(function (e) {
         if (val === "python hello_world.py") {
             $('#screen p').text('hello to you!');
         } else if (val === "ls") {
-            $('#screen p').text("'hello_world.py'");
+            $('#screen p').text("hello_world.py");
         } else if (val == "help") {
             $('#screen p').text('ls - enter to display directory file commands');
         } else {
             if (val.indexOf(".py") != -1) {
                 $('#screen p').text("python (.py) scripts must have prefix command 'python' ---  try 'python <command>'");
             } else {
-                $('#screen p').text('unrecognized command: use "help" command for, well, help!');
+                $('#screen p').text("unrecognized command: use 'help' command for, well, help!");
             }
         }
     }
